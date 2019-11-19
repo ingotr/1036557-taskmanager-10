21f75bcb441937a4764f7c2b3094b2978c5d5970
Aleksandr <markovkin88@gmail.com>
2019-11-19 20:21:16 +0300

    Убирает лишний символ из package.json. Упрощает функции передачи компонентов верстки

--- src/main.js
+++ src/main.js
@@ -2,9 +2,8 @@
 
 const TASK_COUNT = 3;
 
-const createMainMenuTemplate = () => {
-  return (
-    `<section class="control__btn-wrap">
+const createMainMenuTemplate = () => (
+  `<section class="control__btn-wrap">
     <input
       type="radio"
       name="control"
@@ -32,12 +31,10 @@ const createMainMenuTemplate = () => {
       >STATISTICS</label
     >
   </section>`
-  );
-};
+);
 
-const createFilterTemplate = () => {
-  return (
-    `<section class="main__filter filter container">
+const createFilterTemplate = () => (
+  `<section class="main__filter filter container">
       <input
         type="radio"
         id="filter__all"
@@ -105,26 +102,21 @@ const createFilterTemplate = () => {
       >Archive <span class="filter__archive-count">115</span></label
       >
     </section>`
-  );
-};
+);
 
-const createBoardTemplate = () => {
-  return (
-    `<section class="board container">
+const createBoardTemplate = () => (
+  `<section class="board container">
       <div class="board__filter-list">
         <a href="#" class="board__filter">SORT BY DEFAULT</a>
         <a href="#" class="board__filter">SORT BY DATE up</a>
         <a href="#" class="board__filter">SORT BY DATE down</a>
       </div>
       <div class="board__tasks"></div>
-    </section>`
-  );
-};
+  </section>`
+);
 
-const createEditingCardTemplate = () => {
-  return (
-    `
-  <article class="card card--edit card--yellow card--repeat">
+const createEditingCardTemplate = () => (
+  `<article class="card card--edit card--yellow card--repeat">
   <form class="card__form" method="get">
     <div class="card__inner">
       <div class="card__color-bar">
@@ -378,15 +370,11 @@ const createEditingCardTemplate = () => {
       </div>
     </div>
   </form>
-</article>
-  `
-  );
-};
+</article>`
+);
 
-const createDefaultCardTemplate = () => {
-  return (
-    `
-    <article class="card card--black">
+const createDefaultCardTemplate = () => (
+  `<article class="card card--black">
             <div class="card__form">
               <div class="card__inner">
                 <div class="card__control">
@@ -450,16 +438,11 @@ const createDefaultCardTemplate = () => {
                 </div>
               </div>
             </div>
-    </article>
-    `
-  );
-};
+    </article>`
+);
 
-const createLoadMoreButtonTemplate = () => {
-  return (
-    `<button class="load-more" type="button">load more</button>`
-  );
-};
+const createLoadMoreButtonTemplate = () => `<button class="load-more"
+type="button">load more</button>`;
 
 const render = (container, template, place = `beforeEnd`) => {
   container.insertAdjacentHTML(place, template);
