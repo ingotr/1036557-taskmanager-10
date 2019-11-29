7c690861d597e95f3ee51f11ed2a14e2b9af6449
Aleksandr <markovkin88@gmail.com>
2019-11-29 23:27:51 +0300

    Создает моки и вспомогательные файлы

--- webpack.config.js
+++ webpack.config.js
@@ -7,15 +7,16 @@ module.exports = {
   },
   entry: `./src/main.js`,
   output: {
+    // eslint-disable-next-line no-undef
     path: path.join(__dirname, `public`),
     filename: `bundle.js`,
   },
   devtool: `source-map`,
   devServer: {
+    // eslint-disable-next-line no-undef
     contentBase: path.join(__dirname, `public`),
     publicPath: `http:/localhost:8080/`,
     compress: true,
-    watchContenBase: true,
     open: true,
   }
 };
