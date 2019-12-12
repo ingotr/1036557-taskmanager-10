81f5d5c6d7f62c89c52e27df1d43cedee3cf67f0
Markovkin Aleksandr <markovkin88@gmail.com>
2019-12-12 15:21:05 +0300

    Устанавливает обработчики на кнопки карточки и формы редактирования. Делает методы замены карточки на форму приватными

--- src/components/controllers/task.js
+++ src/components/controllers/task.js
@@ -25,33 +25,24 @@ export default class TaskController {
     this._taskComponent = new TaskComponent(task);
     this._taskEditComponent = new TaskEditComponent(task);
 
-    const onEscKeyDown = (evt) => {
-      const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
-      if (isEscKey) {
-        replaceEditToTask();
-        document.removeEventListener(`keydown`, onEscKeyDown);
-      }
-    };
+    this._taskComponent.setEditButtonClickHandler(() => {
+      this._replaceEditToTask();
+      document.addEventListener(`keydown`, this._onEscKeyDown);
+    });
 
-    const replaceEditToTask = () => {
-      replace(taskComponent, taskEditComponent);
-    };
+    this._taskComponent.setArchiveButtonClickHandler(() => {
+      this._onDataChange(this, task, Object.assign({}, task, {
+        isArchive: !task.isArchive,
+      }));
+    });
 
-    const replaceTaskToEdit = () => {
-      replace(taskEditComponent, taskComponent);
-    };
+    this._taskComponent.setFavoritesButtonClickHandler(() => {
+      this._onDataChange(this, task, Object.assign({}, task, {
+        isFavorite: !task.isFavorite
+      }));
+    });
 
-    const editButtonHandler = () => {
-      replaceTaskToEdit();
-      document.addEventListener(`keydown`, onEscKeyDown);
-    };
-
-    const taskComponent = new TaskComponent(task);
-    taskComponent.setEditButtonClickHandler(editButtonHandler);
-
-    const taskEditComponent = new TaskEditComponent(task);
-    taskEditComponent.setSubmitHandler(replaceEditToTask);
-    render(this._container, taskComponent, RenderPosition.BEFOREEND);
+    this._taskEditComponent.setSubmitHandler(() => this._replaceEditToTask());
   }
 
   setDefaultView() {
@@ -59,4 +50,32 @@ export default class TaskController {
       this._replaceEditToTask();
     }
   }
+
+  _replaceEditToTask() {
+    this._taskEditComponent.reset();
+
+    replace(this._taskComponent, this._taskEditComponent);
+    this._mode = Mode.DEFAULT;
+  }
+
+  _replaceTaskToEdit() {
+    this._onViewChange();
+
+    replace(this._taskEditComponent, this._taskComponent);
+    this._mode = Mode.EDIT;
+  }
+
+  _onEscKeyDown(evt) {
+    const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
+
+    if (isEscKey) {
+      this._replaceEditToTask();
+      document.removeEventListener(`keydown`, this._onEscKeyDown);
+    }
+  }
+
+  // const editButtonHandler = () => {
+  //   replaceTaskToEdit();
+  //   document.addEventListener(`keydown`, onEscKeyDown);
+  // };
 }
