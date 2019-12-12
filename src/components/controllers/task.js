5b9d85a96c37505df256d1ec0309fc2b4f188d18
Markovkin Aleksandr <markovkin88@gmail.com>
2019-12-12 15:08:46 +0300

    Добавляет ссылки на старый и новый компонент карточки задачи. Связывает между собой карточку задачи, редактирования и taskController

--- src/components/controllers/task.js
+++ src/components/controllers/task.js
@@ -11,16 +11,27 @@ export default class TaskController {
   constructor(container, onDataChange) {
     this._container = container;
     this._onDataChange = onDataChange;
+
+    this._taskComponent = null;
+    this._taskEditComponent = null;
+
+    this._onEscKeyDown = this._onEscKeyDown.bind(this);
   }
 
   render(task) {
+    const oldTaskComponent = this._taskComponent;
+    const oldTaskEditComponent = this._taskEditComponent;
+
+    this._taskComponent = new TaskComponent(task);
+    this._taskEditComponent = new TaskEditComponent(task);
+
     const onEscKeyDown = (evt) => {
       const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
       if (isEscKey) {
         replaceEditToTask();
         document.removeEventListener(`keydown`, onEscKeyDown);
       }
-    }
+    };
 
     const replaceEditToTask = () => {
       replace(taskComponent, taskEditComponent);
