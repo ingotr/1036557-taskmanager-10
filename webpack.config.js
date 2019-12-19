7dff11be829567bcd536484a62bbbbfab9047b00
Aleksandr <markovkin88@gmail.com>
2019-12-19 22:46:12 +0300

    webpack.config: Обновляет настройки конфига

--- webpack.config.js
+++ webpack.config.js
@@ -7,17 +7,15 @@ module.exports = {
   },
   entry: `./src/main.js`,
   output: {
+    filename: `bundle.js`,
     // eslint-disable-next-line no-undef
     path: path.join(__dirname, `public`),
-    filename: `bundle.js`,
   },
   devtool: `source-map`,
   devServer: {
     // eslint-disable-next-line no-undef
     contentBase: path.join(__dirname, `public`),
-    publicPath: `http:/localhost:8080/`,
     compress: true,
-    open: true,
   },
   module: {
     rules: [
